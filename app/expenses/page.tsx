2babc4f0639dc82c66b0aedf3d2331b605e820f9
louis <louisgituhi@gmail.com>
2024-09-08 11:46:15 +0300

    added time revalidation

--- app/expenses/page.tsx
+++ app/expenses/page.tsx
@@ -2,15 +2,16 @@ import { Payment, columns } from "./columns"
 import { DataTable } from "./data-table"
 import { db } from "@/database/drizzle/db"
 import { expensesTable } from "@/database/drizzle/schema"
-import { revalidatePath } from "next/cache"
 
+
+export const dynamic = "force-dynamic";
+export const revalidate = 6;
 
 async function getData(): Promise<Payment[]> {
   const data = await db.select().from(expensesTable)
-  revalidatePath("/expenses")
 
   return data
-}
+};
 
 export default async function DemoPage() {
   const data = await getData()
